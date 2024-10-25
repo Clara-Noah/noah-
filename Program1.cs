413d6c60bf07f2a1d76ccd92d2fca34d1c520c86
Clara-Noah <claraclaudio806@gmail.com>
2024-10-25 13:06:43 +0100

    Add files via upload

---
+++ Program1.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+public class PrimeiroPrograma
+{
+    public static void Main(string[] args)
+    {
+        //declarar variáveis
+        double raio = 5;
+
+        //Lógica do programa
+
+        double diametro = 2 * raio;
+
+        Console.WriteLine("a divisão do raio é " + diametro);
+
+
+
+
+
+
+
+    }
+
+}
